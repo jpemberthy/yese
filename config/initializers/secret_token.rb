58c3be619494f3645ee6247c86110c5d16daf23c
jpemberthy <jpemberthy@gmail.com>
2012-04-23 18:49:04 -0500

    rico

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yese::Application.config.secret_token = '279a2282eb1447916edb509790eb856d5b5a81051c161a6f7d0498bc7fd76d77e22f126a020ac8019b38a2fed2f870e3c906c0b80d99ee2bb90f31ac485800a1'
